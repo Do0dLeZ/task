a6d3f63d197b7766c6ebc9ced3004a9a2cd27883
Eugene <kolomiets1990@gmail.com>
2018-05-18 20:32:28 +0300

    Finish sign up

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe "User pages", type: :request do
-  subject {page}
+describe "User pages" do
 
-  describe "Profile page" do
-    let(:user) {FactoryGirl.create :user}
-    before {visit user_path(user)}
+  subject { page }
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.full_name) }
+    it { should have_title(user.full_name) }
   end
-  
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
 end
